4a0e99fd4e80360a7c858f61cc3b19d8966938f9
puffeve <jiraphan389@gmail.com>
2024-11-17 17:27:15 +0700

    dashboard

---
+++ my-app/src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import { supabase } from '@/lib/supabase';
+
+function page() {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default page
