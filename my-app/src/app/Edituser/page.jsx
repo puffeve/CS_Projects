2f873b2cc4eec67bff616f8bf4a909c338512ff0
ichxx <94209501+ichxxcho@users.noreply.github.com>
2025-03-07 02:39:05 +0700

    Update page.jsx

--- my-app/src/app/Edituser/page.jsx
+++ my-app/src/app/Edituser/page.jsx
@@ -10,17 +10,19 @@ function Sidebar({ userName, onLogout }) {
             <div>
                 <h1 className="text-2xl font-bold">ClassMood Insight</h1>
                 {userName && <p className="text-lg font-semibold mt-4">สวัสดี {userName}</p>}
-                <hr className="border-sky-300 my-6" />
+                <hr className="border-black my-6" />
                 <nav>
-          <a href="/searchacc" className="block py-2.5 px-4 mt-3 bg-sky-600 hover:bg-sky-400 rounded-lg">จัดการข้อมูลบัญชี</a>
-          <a href="/searchcourse" className="block py-2.5 px-4 mt-3 bg-sky-600 hover:bg-sky-400 rounded-lg">จัดการข้อมูลรายวิชา</a>
+          <a href="/searchacc" className="block py-2.5 px-4 mt-3 bg-pink-400 hover:bg-pink-300 text-white rounded-lg">จัดการข้อมูลบัญชี</a>
+          <a href="/searchcourse" className="block py-2.5 px-4 mt-3 bg-pink-400 hover:bg-pink-300 text-white rounded-lg">จัดการข้อมูลรายวิชา</a>
+          
+
           
         </nav>
             </div>
             <div className="flex-grow"></div>
             <button
                 onClick={onLogout}
-                className="bg-pink-400 text-white px-4 py-2 rounded-lg"
+                className="bg-red-400 active:bg-[#1d2f3f] text-white px-4 py-2 rounded-lg"
             >
                 ออกจากระบบ
             </button>
@@ -98,7 +100,7 @@ export default function EditAccount() {
             console.error('Error updating user data:', error);
             alert('Error saving changes.');
         } else {
-            alert('Changes saved successfully!');
+            alert('บันทึกการเปลี่ยนแปลงสำเร็จแล้ว!');
         }
     };
 
@@ -156,7 +158,7 @@ export default function EditAccount() {
                                 className="w-full px-4 py-3 border rounded-md"
                             >
                                 <option value="teacher">อาจารย์ผู้สอน</option>
-                                <option value="student">ผู้เรียน</option>
+                               
                                 <option value="admin">ผู้ดูแลระบบ</option>
                             </select>
                         </div>
