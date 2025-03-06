7f09276f10b587149d4bd51eb1292de5e3023d47
ichxx <94209501+ichxxcho@users.noreply.github.com>
2025-03-07 02:40:35 +0700

    Update page.jsx

--- my-app/src/app/searchcourse/page.jsx
+++ my-app/src/app/searchcourse/page.jsx
@@ -104,16 +104,16 @@ export default function AccountManagement() {
         <div className="p-4">
           <h1 className="text-2xl font-bold">ClassMood Insight</h1>
           {userName && <div className="text-lg font-semibold mt-4">สวัสดี {userName}</div>}
-          <hr className="border-sky-300 my-6" />
+          <hr className="border-black my-6" />
           <nav>
-            <a href="/searchacc" className="block py-2.5 px-4 mt-3 bg-sky-600 hover:bg-sky-400 rounded-lg">จัดการข้อมูลบัญชี</a>
-            <a href="/searchcourse" className="block py-2.5 px-4 mt-3 bg-sky-600 hover:bg-sky-400 rounded-lg">จัดการข้อมูลรายวิชา</a>
+            <a href="/searchacc" className="block py-2.5 px-4 mt-3 bg-pink-400 hover:bg-pink-300 text-white rounded-lg">จัดการข้อมูลบัญชี</a>
+            <a href="/searchcourse" className="block py-2.5 px-4 mt-3 bg-pink-400 hover:bg-pink-300 text-white rounded-lg">จัดการข้อมูลรายวิชา</a>
             
           </nav>
         </div>
         <button
           onClick={handleLogout}
-          className="bg-pink-400 text-black px-4 py-2 m-4 rounded-lg"
+          className="bg-red-400 active:bg-[#1d2f3f] text-white px-4 py-2 m-4 rounded-lg"
         >
           ออกจากระบบ
         </button>
